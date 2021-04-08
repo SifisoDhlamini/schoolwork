06d9095fe14ad319f50c7f994fe42dc4f1fbb9cd
Sifiso Lucolo Dhlamini <66865570+SifisoDhlamini@users.noreply.github.com>
2021-04-08 20:21:59 +0800

    Question 19 complete & Q18 adjusted

--- question19/main.cpp
+++ question19/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-#include <ctype.h>
+#include <vector>
+#include <math.h>
+#include <string>
+#include <cstdlib>
+#include <cctype>
+#include <algorithm>
 
 using namespace std;
 
@@ -27,7 +32,7 @@ public:
         }
     }
     
-    int getDigit(){
+    int getDigit() const{
         return digit;
     }
 };
@@ -48,15 +53,25 @@ public:
         int counter = 0;
         
         if(isdigit(not_string[0])){
-            //number = stoll(not_string);            
-            while(isdigit(not_string[counter])){
-                number[arrCounter].setDigit(not_string[counter] - '0');
+            //if there is more than 1 saved Digit and it starts with 0  
+            while(not_string[counter] - '0' == 0){
                 counter++;
-                arrCounter++;
             }
+            if(counter > 1 || not_string[counter] - '0' != 0){
+                while(isdigit(not_string[counter])){
+                    number[arrCounter].setDigit(not_string[counter] - '0');
+                    counter++;
+                    arrCounter++;
+                }
+            }
+            
         } else if((not_string[0] == '+' || not_string[0] == '-') && isdigit(not_string[1])){
             //move one up
             counter = 1;
+            //if there is more than 1 saved Digit and it starts with 0  
+             while(not_string[counter] - '0' == 0){
+                counter++;
+            } 
             //set first number to negative if need be
             if(not_string[0] == '-'){
                 number[arrCounter].setDigit(-1 * (not_string[counter] - '0'));
@@ -76,29 +91,184 @@ public:
     }
     
     void display(){
-        int i = 0;
-        //if there is more than 1 saved Digit and it starts with 0
-        if(arrCounter > 1){
-            while(number[i].getDigit() == 0){
-                i++;
-            }  
-        }        
-        
-        //print
-        for(;i < arrCounter; i++){
-            cout<<number[i].getDigit();
+        if(arrCounter > 0){
+            for(int i = 0;i < arrCounter; i++){
+                cout<<number[i].getDigit();
+            }   
+        } else {
+            cout<<0;
         }
-        cout<<endl;        
+             
     } 
-
-    IntNumber *add(const IntNumber *n) const;
     
+    int getSize() const{return arrCounter;}
+
+    IntNumber *add(const IntNumber *otherLargeInt) const{
+        vector <int> num;
+        int remainder = 0;
+        int counter = 1;
+        
+        //get size of each largeInt
+        int size1 = otherLargeInt->getSize();
+        int size2 = this->getSize();
+        
+        //create temp arrays for addition purposes
+        Digit temp1[size1];
+        Digit temp2[size2];
+        
+        //check if negative for passed in Digit array
+        if(otherLargeInt->number[0].getDigit()< 0){
+            temp1[0].setDigit(otherLargeInt->number[0].getDigit());
+            for(int i = 1; i < size1;i++){
+                temp1[i].setDigit(otherLargeInt->number[i].getDigit()*(-1));
+            }
+        } else {
+            for(int i = 0; i < size1;i++){
+                temp1[i].setDigit(otherLargeInt->number[i].getDigit());
+            }
+        }
+        //check if negative for local Digit array
+        if(this->number[0].getDigit() < 0){
+            temp2[0].setDigit(this->number[0].getDigit());
+            for(int i = 1; i < size2;i++){
+                temp2[i].setDigit(this->number[i].getDigit()*(-1));
+            }
+        } else {
+            for(int i = 0; i < size2;i++){
+                temp2[i].setDigit(this->number[i].getDigit());
+            }
+        }
+        
+        if(size1 >= size2){
+            while(counter <= size2){
+                int val1 = temp1[size1 - counter].getDigit();
+                int val2 = temp2[size2 - counter].getDigit();
+                int sum = val1 + val2 + remainder;
+                if(sum >= 10){
+                    remainder = 1;
+                    num.push_back(sum - 10); 
+                } else if(sum <= (-10)){
+                    remainder = -1;
+                    num.push_back(sum  + 10);
+                } else {
+                    remainder = 0;
+                    num.push_back(sum);
+                }   
+                counter++;
+            } 
+            while(counter <= size1){
+                int val1 = temp1[size1 - counter].getDigit();
+                int sum = val1+remainder;
+                if(sum >= 10){
+                    remainder = 1;
+                    num.push_back(sum <= 0 && sum > -10); 
+                } else if(sum < (-10)){
+                    remainder = -1;
+                    num.push_back(sum  + 10);                    
+                } else {
+                    remainder = 0;
+                    num.push_back(sum);
+                } 
+                counter++;
+            }
+            if(remainder !=0){
+                num.push_back(remainder);
+            }
+            counter = 1;
+        } else {
+            while(counter <= size1){
+                int val1 = temp1[size1 - counter].getDigit();
+                int val2 = temp2[size2 - counter].getDigit();
+                int sum = val2 + val1 + remainder;
+                if(sum >= 10){
+                    remainder = 1;
+                    num.push_back(sum - 10); 
+                } else if(sum <= (-10)){
+                    remainder = -1;
+                    num.push_back(sum  + 10);
+                } else {
+                    remainder = 0;
+                    num.push_back(sum);
+                }   
+                counter++;
+            } 
+            while(counter <= size2){
+                int val2 = temp2[size2 - counter].getDigit();
+                int sum = val2+remainder;
+                if(sum >= 10){
+                    remainder = 1;
+                    num.push_back(sum - 10); 
+                } else if(sum <= (-10)){
+                    remainder = -1;
+                    num.push_back(sum  + 10);                    
+                } else {
+                    remainder = 0;
+                    num.push_back(sum);
+                } 
+                counter++;
+            }
+            if(remainder != 0){
+                num.push_back(remainder);
+            }
+            counter = 1;
+        }
+
+        //save vector integers in to character array to pass as parameter to IntLargeNumber constructor
+        
+        int vecSize = num.size();        
+        reverse(num.begin(), num.end());
+        while(num.at(0) == 0 && vecSize > 1){
+            num.erase(num.begin());
+            vecSize--;
+        }
+        char newNum[vecSize + 2];
+        if(vecSize > 1 && num.at(0) > 0){
+            for(int i = 0; i < vecSize; i++){
+                if((i > 0) && (num.at(i) < 0)){
+                    num.at(i - 1)-= 1;
+                    num.at(i)+=10;
+                    newNum[i - 1] = '0' + num.at(i - 1);
+                }
+                newNum[i] = '0' + num.at(i);
+            } 
+           newNum[vecSize] ='\0';
+        } else if(vecSize > 1 && num.at(0) < 0){
+            int insertPos = 0;
+            newNum[insertPos] = '-';
+            insertPos++;
+            for(int i = 0; i < vecSize; i++){
+                if((i > 0) && (num.at(i) < 0)){
+                    num.at(i - 1)-= 1;
+                    num.at(i)+=10;
+                    newNum[insertPos - 1] = '0' + num.at(i - 1);
+                }
+                newNum[insertPos] = '0' + abs(num.at(i));
+                insertPos++;
+            }
+            newNum[insertPos] ='\0';
+        } else {
+            if (num.at(0) >= 0){
+                newNum[0] = '0' + num.at(0);
+                newNum[1] = '\0';
+            } else {
+                newNum[0] = '-';
+                newNum[1] = '0' + abs(num.at(0));
+                newNum[2] = '\0'; 
+            }            
+        }
+        
+        char copyN[1000];
+        int copyCount = 0;
+        while(newNum[copyCount] != '\0'){
+            copyN[copyCount] = newNum[copyCount];
+            copyCount++;
+        }
+            
+        
+        IntNumber *newNumber = new IntNumber(copyN);       
+        return newNumber;                
+    }
 };
-
-IntNumber *IntNumber::add(const IntNumber *n) const{
-    
-    return nullptr;
-}
 
 int main()
 {
