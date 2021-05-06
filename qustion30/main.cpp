f54ad5195142a4e0a70e236c551ab45e277941dd
Sifiso Lucolo Dhlamini <66865570+SifisoDhlamini@users.noreply.github.com>
2021-05-06 18:21:32 +0800

    Incomplete number 30

--- qustion30/main.cpp
+++ qustion30/main.cpp
@@ -1,9 +1,57 @@
+// not my code
+
 #include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
 using namespace std;
 
-int main(int argc, char **argv)
+int main()
 {
-	cout<<"Hello World\n";
+    string c, m, word;
+    vector<vector<string>> mc;
+    vector <string> charac;
+    vector <string> cmors;
     
-	return 0;
+    //collect characters and save with corresponding mors
+    while (cin >> c && c != "*")
+    {
+        charac.push_back(c);
+        cin >> m;
+        cmors.push_back(m);        
+    }
+    mc.push_back(charac);
+    mc.push_back(cmors)
+    vector<vector<string>> words;
+    vector <string> str1;
+    vector <string> smors;
+    
+    //collect words and save with corresponding mors
+    while (cin >> word && word != "*")
+    {
+        m.clear()
+        str1.push_back(word);
+        for(auto letter: word){
+            auto index = find(cmors.at(0).begin(), cmors.at(0).end(), letter);
+            m+=mc.at(1).at(index);
+        }
+        smors.push_back(m);
+    }
+    words.push_back(str1);
+    words.push_back(smors);
+    
+    //collect code
+    vector <string> morscode;
+    while (cin >> m && m != "*"){
+        morscode.push_back(m);
+    }
+    
+    for(auto code: morscode){
+        auto index = find(mc.at(1).begin(), mc.at(1).begin(),
+        
+    }
+        
+
+    return 0;
 }
